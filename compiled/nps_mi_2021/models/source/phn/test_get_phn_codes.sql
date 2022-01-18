7f0ea0374abb835925e3a7a0d5b934b8ccc15554
pratikcgd <86762371+pratikcgd@users.noreply.github.com>
2022-01-18 09:59:05 +0530

    #2

--- compiled/nps_mi_2021/models/source/phn/test_get_phn_codes.sql
+++
@@ -1,8 +0,0 @@
-
-
-
-
-
-
-select distinct PHN_CODE_2017 from `nps-omop-project`.`C_SREDH_NPS_MI_STAGING_SIMPLIFIED_2021`.`PRIMARY_HEALTH_NETWORK` order by PHN_CODE_2017 
-
--- compiled/nps_mi_2021/models/staging/phn/PRIMARY_HEALTH_NETWORK.sql
+++
@@ -1,4 +0,0 @@
-
-
-select *
-from `nps-omop-project`.`B_SREDH_NPS_MI_SF_RDV_BDV_2021`.`PHNCODE_DATA`
